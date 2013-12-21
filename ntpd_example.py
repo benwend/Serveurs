28e4a44babf7b60f883c1115a119106daa61c30f
Benjamin Wendling <benjamin.wend+git@gmail.com>
2013-12-21 16:26:30 +0100

    Move scripts to jail service in other repository

--- ntpd_example.py
+++
@@ -1,4 +0,0 @@
-chroot="/var/chroot/ntpd"
-testCommandsInsideJail=["start-stop-daemon --start --quiet --exec /usr/sbin/ntpd"]
-processNames=["ntpd"]
-testCommandsOutsideJail=["ntpdate -q 127.0.0.1"]
