342a332c9f080afeeb636614da29f66338db93df
benwend <benjamin.wend+git@gmail.com>
2016-03-12 11:47:19 +0100

    Màj install_openVAS.sh
    Nouvelles versions
    Fixbugs

--- Scripts/install_openVAS.sh
+++ Scripts/install_openVAS.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-
+#
+################################################################################
+#
+# Author  : benwend <benjamin.wend+git@gmail.com>
+# Date    : 12/03/2016
+# Version : 0.4
+#
+################################################################################
+# DOC :
+#	http://www.openvas.org/install-source.html
+#	https://github.com/ChrisFernandez/openvas_install/blob/master/install_openvas.sh
+################################################################################
+# 11/03/2016	benwend		Initial release (v0.1)
+# 12/03/2016	benwend		Create 'install' function && add page ID (v0.2)
+# 12/03/2016	benwend		Test if the directory '/opt/openvas' exists (v0.3)
+# 12/03/2016	benwend		Fix bugs (v0.4)
+################################################################################
 
 SMBID="1975"
 SMB="openvas-smb-1.0.1"
@@ -14,31 +30,38 @@ GSA="greenbone-security-assistant-6.0.10"
 CLID="2209"
 CLI="openvas-cli-1.4.3"
 
-FOLDER="/opt/openvas"
+DIR="/opt/openvas"
 
-mkdir $FOLDER && cd $FOLDER
+if [ ! -d "$DIR" ]; then
+	echo -e "No directory '/opt/openvas' !"
+	echo -e "Exec the cmd \"sudo mkdir $DIR && sudo chown <USERNAME>:<USERNAME> $DIR\""
+	echo -e "Goodbye"
+	exit 1
+else
+	cd $DIR
+fi
 
 function install() {
 	ID=$1
 	PK=$2
-	
-	cd $FOLDER
+
+	cd $DIR
 
 	# Téléchargement/Décompression/Nettoyage des sources
 	if [ ! -f "$PK.tar.gz" ]; then
-		echo "\n* DOWNLOAD '$PK' :"
+		echo -e "\n* DOWNLOAD '$PK' :"
 		wget http://wald.intevation.org/frs/download.php/$SMBID/$SMB.tar.gz
-		echo "\n* Untar '$PK.tar.gz' :"
+		echo -e "\n* Untar '$PK.tar.gz' :"
 		tar xzf $PK.tar.gz
-		echo "\n* Remove '$PK.tar.gz' :"
+		echo -e "\n* Remove '$PK.tar.gz' :"
 		rm $PK.tar.gz
   	fi
 
-	echo "BUILD $PK :"
+	echo -e "BUILD $PK :"
 	cd $PK
 
 	if [ -d "build" ]; then
-		echo "\n\t* Remove old build/ of '$PK' :"
+		echo -e "\n\t* Remove old build/ of '$PK' :"
 		rm -rf build
 	fi
 
@@ -51,22 +74,22 @@ function install() {
 	make install
 	make rebuild_cache
 
-	echo "CLEAN $PK :"
+	echo -e "CLEAN $PK :"
 	cd .. && rm -rf build
 }
 
 
-echo "Installing needed packages :"
-sudo apt install -y gcc wget curl make cmake build-essential pkg-config fakeroot nmap\
+echo -e "Installing needed packages :"
+sudo apt install -y gcc wget curl make cmake build-essential pkg-config fakeroot nmap \
 bison flex libgnutls28-dev libglib2.0-dev libssh-dev libpcap-dev libhiredis-dev uuid-dev libgpgme11-dev libgcrypt20-dev libldap2-dev libksba-dev libpopt-dev heimdal-multidev mingw32 libmicrohttpd-dev \
 redis-server sqlite3 libsqlite3-dev \
 doxygen libxml2-dev libxslt1-dev xmltoman xsltproc libxml2-dev
-					  
 
-echo "Exporting PGK_CONFIG_PATH :"
+echo -e "Exporting PGK_CONFIG_PATH :"
 export PKG_CONFIG_PATH=/opt/openvas/lib/pkgconfig
-mkdir -p $PKG_CONFIG_PATH
-
+if [ ! -d "$PKG_CONFIG_PATH" ]; then
+	mkdir -p $PKG_CONFIG_PATH
+fi
 
 install $SMBID $SMB
 
