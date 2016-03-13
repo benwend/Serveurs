8bcd940c85897b5a8e1d276656c72afbec3d3e61
benwend <benjamin.wend+git@gmail.com>
2016-03-13 15:03:48 +0100

    Màj installation d'OpenVAS

--- Scripts/install_openVAS.sh
+++ Scripts/install_openVAS.sh
@@ -3,19 +3,21 @@
 ################################################################################
 #
 # Author  : benwend <benjamin.wend+git@gmail.com>
-# Date    : 12/03/2016
-# Version : 0.4
+# Date    : 13/03/2016
+# Version : 0.5
 #
 ################################################################################
 # DOC :
 #	http://www.openvas.org/install-source.html
 #	https://github.com/ChrisFernandez/openvas_install/blob/master/install_openvas.sh
+#	lab_-_openvas.pdf
 ################################################################################
 # 11/03/2016	benwend		Initial release (v0.1)
 # 12/03/2016	benwend		Create 'install' function && add page ID (v0.2)
 # 12/03/2016	benwend		Test if the directory '/opt/openvas' exists (v0.3)
 # 12/03/2016	benwend		Fix bugs (v0.4)
 # 12/03/2016	benwend		DL CLI-1.4.2 : Bug sur Debian 8 avec 1.4.3
+# 13/03/2016	benwend		Init OpenVAS (In going -- v0.5)
 ################################################################################
 
 SMBID="1975"
@@ -28,21 +30,11 @@ MANID="2295"
 MANAGER="openvas-manager-6.0.8"
 GSAID="2299"
 GSA="greenbone-security-assistant-6.0.10"
-
 # BUG de la 1.4.3 sur debian 8 
 CLID="2141"
 CLI="openvas-cli-1.4.2"
 
 DIR="/opt/openvas"
-
-if [ ! -d "$DIR" ]; then
-	echo -e "No directory '/opt/openvas' !"
-	echo -e "Exec the cmd \"sudo mkdir $DIR && sudo chown <USERNAME>:<USERNAME> $DIR\""
-	echo -e "Goodbye"
-	exit 1
-else
-	cd $DIR
-fi
 
 function install() {
 	ID=$1
@@ -52,19 +44,19 @@ function install() {
 
 	# Téléchargement/Décompression/Nettoyage des sources
 	if [ ! -f "$PK.tar.gz" ]; then
-		echo -e "\n* DOWNLOAD '$PK' :"
+		echo -e "\n* DOWNLOADING '$PK'"
 		wget http://wald.intevation.org/frs/download.php/$ID/$PK.tar.gz
-		echo -e "\n* Untar '$PK.tar.gz' :"
+		echo -e "\n* Untaring '$PK.tar.gz'"
 		tar xzf $PK.tar.gz
-		echo -e "\n* Remove '$PK.tar.gz' :"
+		echo -e "\n* Removing '$PK.tar.gz'"
 		rm $PK.tar.gz
   	fi
 
-	echo -e "BUILD $PK :"
+	echo -e "\n* BUILDING $PK"
 	cd $PK
 
 	if [ -d "build" ]; then
-		echo -e "\n\t* Remove old build/ of '$PK' :"
+		echo -e "\n\t* Removing old build/ of '$PK'"
 		rm -rf build
 	fi
 
@@ -77,18 +69,26 @@ function install() {
 	make install
 	make rebuild_cache
 
-	echo -e "CLEAN $PK :"
+	echo -e "\n* CLEANNING $PK"
 	cd .. && rm -rf build
 }
 
 
-echo -e "Installing needed packages :"
-sudo apt install -y gcc wget curl make cmake build-essential pkg-config fakeroot nmap \
-bison flex libgnutls28-dev libglib2.0-dev libssh-dev libpcap-dev libhiredis-dev uuid-dev libgpgme11-dev libgcrypt20-dev libldap2-dev libksba-dev libpopt-dev heimdal-multidev mingw32 libmicrohttpd-dev \
-redis-server sqlite3 libsqlite3-dev \
-doxygen libxml2-dev libxslt1-dev xmltoman xsltproc libxml2-dev
+if [ ! -d "$DIR" ]; then
+	sudo 
+else
+	cd $DIR
+fi
 
-echo -e "Exporting PGK_CONFIG_PATH :"
+echo -e "\n* Installing needed packages :"
+sudo apt install -y \
+gcc wget make cmake build-essential autoconf pkg-config fakeroot alien nsis \
+bison flex uuid-dev mingw32 \
+libglib2.0-dev libgnutls28-dev libpcap-dev libgpgme11-dev libssh-dev libldap2-dev libmicrohttpd-dev libgcrypt20-dev libpopt-dev heimdal-multidev \
+redis-server libhiredis-dev sqlite3 libsqlite3-dev \
+libxml2-dev libxslt1-dev xsltproc doxygen xmltoman
+
+echo -e "\n* Exporting PGK_CONFIG_PATH :"
 export PKG_CONFIG_PATH=/opt/openvas/lib/pkgconfig
 if [ ! -d "$PKG_CONFIG_PATH" ]; then
 	mkdir -p $PKG_CONFIG_PATH
@@ -105,3 +105,58 @@ install $MANID $MANAGER
 install $GSAID $GSA
 
 install $CLID $CLI
+
+
+echo -e "\n* Adding openvas to the enviroment PATH"
+export PATH=/opt/openvas/bin:/opt/openvas/sbin:$PATH
+sudo sh -c "echo 'export PATH=/opt/openvas/bin:/opt/openvas/sbin:$PATH' >> /etc/bash.bashrc" 
+
+sudo sh -c "echo '/opt/openvas/lib' > /etc/ld.so.conf.d/openvas"
+sudo sh -c "echo '/opt/openvas/lib' >> /etc/ld.so.conf"
+sudo ldconfig
+
+
+#configure
+
+echo -e "\n* CONFIGURE"
+
+echo -e "\n* Creating cert for server :"
+sudo /opt/openvas/sbin/openvas-mkcert
+
+echo -e "\n* Sync NVT :"
+sudo -b env  PATH="/opt/openvas/bin:/opt/openvas/sbin:$PATH" /opt/openvas/sbin/openvas-nvt-sync
+
+echo -e "\n* Creating cert for client :"
+sudo -b env  PATH="/opt/openvas/bin:/opt/openvas/sbin:$PATH" /opt/openvas/sbin/openvas-mkcert-client -n -i
+
+echo -e "\n* Starting the scanner"
+sudo -b env  PATH="/opt/openvas/bin:/opt/openvas/sbin:$PATH" /opt/openvas/sbin/openvassd
+
+echo -e "\n* Rebuilding OpenVASmd"
+sudo /opt/openvas/sbin/openvasmd --rebuild
+
+echo -e "\n* Doing the ScapData Sync"
+sudo -b env  PATH="/opt/openvas/bin:/opt/openvas/sbin:$PATH" /opt/openvas/sbin/openvas-scapdata-sync
+
+echo -e "\n* Doing the CertData sync"
+sudo -b env  PATH="/opt/openvas/bin:/opt/openvas/sbin:$PATH" /opt/openvas/sbin/openvas-certdata-sync
+
+if [ ! -f "/opt/openvas/etc/openvas/pwpolicy.conf" ]; then
+  echo -e "\n* Creating password policy file, read the doc and edit it as you need"
+  sudo touch /opt/openvas/etc/openvas/pwpolicy.conf
+fi
+
+echo -e "\n* Starting openvas manager"
+sudo -b env  PATH="/opt/openvas/bin:/opt/openvas/sbin:$PATH" /opt/openvas/sbin/openvasmd
+
+echo -e "\n* Starting GreenBone security assistant"
+sudo -b env  PATH="/opt/openvas/bin:/opt/openvas/sbin:$PATH" /opt/openvas/sbin/gsad
+
+echo -e "\n* Create config file"
+sudo -b env  PATH="/opt/openvas/bin:/opt/openvas/sbin:$PATH" openvassd -s > /opt/openvas/etc/openvas/openvassd.conf
+
+echo -e "\n* Create your first user :"
+echo -e "\n* openvasmd --first-user=myuser"
+
+echo -e "\n* if any issues download and run :"
+echo -e "\n* wget --no-check-certificate https://svn.wald.intevation.org/svn/openvas/trunk/tools/openvas-check-setup"
